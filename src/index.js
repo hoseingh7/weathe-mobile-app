a9b9b1414b8d4027f670d99f9ef079a8c4cbd2cd
hoseingh7 <hoseingholamii1376@gmail.com>
2023-11-15 23:01:07 -0800

    adding some files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import WeatherAPPProvider from "./Context/Context";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <WeatherAPPProvider>
+      <App />
+    </WeatherAPPProvider>
   </React.StrictMode>
 );
 
